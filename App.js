4d613bfd417478dd67ef3f28cd260ab862b12c41
bala <balu391balu@gmail.com>
2021-12-24 14:50:10 +0530

    f

---
+++ App.js
@@ -0,0 +1,14 @@
+
+
+import './App.css';
+import StudentComponent from './StudentComponent';
+
+function App() {
+  return (
+    <div >
+    <StudentComponent/>
+    </div>
+  );
+}
+
+export default App;
